a59c48fa872fd59480ff1a795d7e18191200bfe9
Maiadee <maiadeemmony@hotmail.co.uk>
2025-01-17 15:57:27 +0000

    added nav bar, log in and sign up files

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,22 @@
-import express from "express"
+import express from "express";
+import User from "../models/user.js";
 
-const router = express.Router()
+const router = express.Router();
 
+// GET log in page
 
+router.route("/user/login").get(async function (req, res, next) {
+  try {
+    res.render("user/login.ejs");
+  } catch (e) {}
+});
 
-export default router
+// GET sign up page
 
+router.route("/user/new").get(async function (req, res, next) {
+  try {
+    res.render("user/signup.ejs");
+  } catch (e) {}
+});
+
+export default router;
